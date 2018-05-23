2e3852f1697a5c386b134fea34d458d434793fbf
rido zaen <ridozaen@irnas-MBP-2.localdomain>
2018-05-23 11:23:58 +0700

    add column userId

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const routes = require('./routes');
+const register = require('./routes/register');
 
 const app = express();
 
 app.use(bodyParser.urlencoded({extended: false}));
 app.set('view engine', 'ejs');
 app.use('/', routes);
+app.use('/register', register);
 
 
 app.listen(3000, ()=>{
--- models/transaction.js
+++ models/transaction.js
@@ -1,7 +1,8 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
   var Transaction = sequelize.define('Transaction', {
-    transactionDate: DataTypes.DATE
+    transactionDate: DataTypes.DATE,
+    userId: DataTypes.INTEGER
   }, {});
   Transaction.associate = function(models) {
     // associations can be defined here
